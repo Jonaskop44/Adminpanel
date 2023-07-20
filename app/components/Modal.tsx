01b18198337ab18c8e130c789b320213b9e7168a
Jonaskop44 <jonas@codeflexx.com>
2023-07-20 23:05:57 +0200

    Add Modul

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,16 +1,23 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from "react";
+import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import * as IconAi from "react-icons/ai";
+import * as IconFa from "react-icons/fa";
 
-const Model = () => {
-  const [open, setOpen] = useState(true);
+interface ModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  title: string;
+  description: string;
+}
+
+const Model: React.FC<ModalProps> = ({ open, setOpen, title, description }) => {
+  const cancelButtonRef = useRef(null);
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
         className="fixed z-10 inset-0 overflow-y-auto"
+        initialFocus={cancelButtonRef}
         onClose={setOpen}
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -42,10 +49,10 @@ const Model = () => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
               <div>
                 <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                  <IconAi.AiFillAccountBook
+                  <IconFa.FaServer
                     className="h-6 w-6 text-green-600"
                     aria-hidden="true"
                   />
@@ -55,23 +62,31 @@ const Model = () => {
                     as="h3"
                     className="text-lg leading-6 font-medium text-gray-900"
                   >
-                    Payment successful
+                    {title}
                   </Dialog.Title>
                   <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Consequatur amet labore.
-                    </p>
+                    <p className="text-sm text-gray-500">{description}</p>
                   </div>
                 </div>
+                <div className="flex justify-center items-center mt-10">
+                  <input type="text" placeholder="test1234" />
+                </div>
               </div>
-              <div className="mt-5 sm:mt-6">
+              <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
                   onClick={() => setOpen(false)}
                 >
-                  Go back to dashboard
+                  Deactivate
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                  onClick={() => setOpen(false)}
+                  ref={cancelButtonRef}
+                >
+                  Cancel
                 </button>
               </div>
             </div>
--- app/dashboard/pages/licenses/server/page.tsx
+++ app/dashboard/pages/licenses/server/page.tsx
@@ -1,11 +1,28 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import Model from "@/app/components/Modal";
 
 const Server = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  useEffect(() => {
+    document.documentElement.style.overflow = "hidden";
+    return () => {
+      document.documentElement.style.overflow = "visible";
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="m-0 h-full">
       <div className="flex justify-end items-start">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          className="mt-4 mr-4 bg-blue-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-xl"
+          onClick={handleOpen}
+        >
           Add Server
         </button>
       </div>
@@ -14,6 +31,12 @@ const Server = () => {
           <h1 className="text-gray-800">Server</h1>
         </div>
       </div>
+      <Model
+        open={open}
+        setOpen={setOpen}
+        title="Enter Sever Details"
+        description="Fill out the formula"
+      />
     </div>
   );
 };
